55b93a5600e10e52d8f4d371d608e4e6cfcacfac
JuliaHwang <qufskan9396@gmail.com>
2017-06-14 22:29:24 +0900

    add model in admin module, created superuser

--- django_app/post/admin.py
+++ django_app/post/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from post.models import Post
+
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, PostAdmin)
