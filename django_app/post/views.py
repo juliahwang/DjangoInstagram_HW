0f5a22ad16410594a17230d4e1aab102988d84f2
JuliaHwang <qufskan9396@gmail.com>
2017-06-14 22:16:40 +0900

    add urls module in post app, created root url pattern in config app, and create basic views

--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'post/post_list.html', context=context)
+
+
+def post_detail(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    context = {
+        'post': post,
+    }
+    return render(request, 'post/post_detail.html', context=context)
