3cfbab40bbfbf2a484db027bb3d7f3058d2365ae
JuliaHwang <qufskan9396@gmail.com>
2017-06-15 00:55:43 +0900

    add post_create url, view and template, add button in post_list template

--- django_app/post/urls.py
+++ django_app/post/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'post'
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^(?P<post_pk>\d+)/$', views.post_detail, name='post_detail')
+    url(r'^(?P<post_pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^create/$', views.post_create, name='post_create'),
 ]
--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+
+from member.models import User
 from .models import Post
 
 # Create your views here.
@@ -23,3 +25,24 @@ def post_detail(request, post_pk):
     }
     # 요청이 들어오면 post/post_detail.html에 context의 데이터를 렌더링해주는 함수.
     return render(request, 'post/post_detail.html', context=context)
+
+
+def post_create(request):
+    # request.method가 'GET'방식일 때
+    if request.method == 'GET':
+        context = {
+        }
+        # 단순히 post_create 템플릿을 렌더링해 보여준다.
+        return render(request, 'post/post_create.html', context)
+    # request.method가 'POST'방식일 때
+    else:
+        # post 생성
+        post = Post.objects.create(
+            author=User.objects.first(),
+            # photo는 request의 FILES 딕셔너리에서 인풋 태그의 이름이
+            # 'file'인 것을 찾아 할당해준다.
+            photo=request.FILES['file'],
+        )
+        # reverse()함수를 사용하여 url을 만들고 해당 페이지로 렌더링할 경우
+        # redirect(<이동할 url이름>, <추가인자>)함수를 사용한다
+        return redirect('post:post_detail', post_pk=post.pk)
